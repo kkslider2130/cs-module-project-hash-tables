29b8088493fcb8fa11583bf39e700ad11c247115
Shun Chiang <yuushamenma@gmail.com>
2020-08-03 16:32:00 -0500

    finished day 3

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,23 @@
 # Your code here
 
+seq = {}
+
 
 def expensive_seq(x, y, z):
     # Your code here
-
+    step_one = (x-1, y+1, z)
+    step_two = (x-2, y+2, z*2)
+    step_three = (x-3, y+3, z*3)
+    if x <= 0:
+        return y+z
+    if x > 0:
+        if step_one not in seq.keys():
+            seq[step_one] = expensive_seq(x-1, y+1, z)
+        if step_two not in seq.keys():
+            seq[step_two] = expensive_seq(x-2, y+2, z*2)
+        if step_three not in seq.keys():
+            seq[step_three] = expensive_seq(x-3, y+3, z*3)
+    return seq[step_one]+seq[step_two]+seq[step_three]
 
 
 if __name__ == "__main__":
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,24 @@
+import re
+d = {}
+
+
 def word_count(s):
     # Your code here
-
+    global d
+    filtered_str = re.sub(r"[^\w\d'\s]", '', s)
+    for word in filtered_str.lower().split():
+        if word not in d.keys():
+            d[word] = 1
+        else:
+            d[word] += 1
+    s = d
+    d = {}
+    return s
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
